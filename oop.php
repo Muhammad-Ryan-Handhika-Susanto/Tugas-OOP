9f45ccd1fe7e4c0566e88934c53176e0a08f9321
Muhammad-Ryan-Handhika-Susanto <110748216+Muhammad-Ryan-Handhika-Susanto@users.noreply.github.com>
2023-05-15 11:24:39 +0700

    Update oop.php

--- oop.php
+++ oop.php
@@ -20,3 +20,28 @@ $mahasiswa1->tampil_biodata('190106', 'Muhammad Ryan Handhika Susanto', 'RPL');
 echo "NIM: " . $mahasiswa1->nim . "<br>";
 echo "Nama: " . $mahasiswa1->nama . "<br>";
 echo "Jurusan: " . $mahasiswa1->jurusan . "<br>";
+
+// Menggunakan method constructor
+
+class mahasiswa {
+    // properti
+    public $nim;
+    public $nama;
+    public $jurusan;
+    
+    function __construct($nim,$nama,$jurusan){
+        $this->nim = $nim;
+        $this->nama = $nama;
+        $this->jurusan = $jurusan;
+    }
+    
+    function tampil_biodata() {
+        echo "NIK : " . $this->nim;
+        echo "<br>";
+        echo "NAMA : " . $this->nama;
+        echo "<br>";
+        echo "JURUSAN : " . $this->jurusan;
+    }
+}    
+$mahasiswa1 = new mahasiswa('190106', 'Muhammad Ryan Handhika Susanto', 'RPL');
+$mahasiswa1->tampil_biodata();
